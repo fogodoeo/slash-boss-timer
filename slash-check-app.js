152473d67122cc584e920c37913276df8c3b2c7a
fogodoeo <fogodoeo@example.com>
2026-05-09 09:25:07 +0900

    Serve svg favicon with correct mime type

--- slash-check-app.js
+++ slash-check-app.js
@@ -16,6 +16,7 @@ const mimeTypes = {
     '.css': 'text/css; charset=utf-8',
     '.js': 'text/javascript; charset=utf-8',
     '.json': 'application/json; charset=utf-8',
+    '.svg': 'image/svg+xml; charset=utf-8',
     '.ico': 'image/x-icon'
 };
 
